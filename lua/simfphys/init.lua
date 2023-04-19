adcb27971d785506bd7939357a9c31319eca1728
Luna <swagnemite@gmx.de>
2023-04-19 20:51:58 +0200

    entity register test

--- lua/simfphys/init.lua
+++ lua/simfphys/init.lua
@@ -20,8 +20,6 @@ if SERVER then
 end
 	
 if CLIENT then
-	killicon.Add( "gmod_sent_vehicle_fphysics_base", "HUD/killicons/simfphys_car", Color( 255, 80, 0, 255 ) )
-
 	include("simfphys/base_functions.lua")
 	include("simfphys/client/fonts.lua")
 	include("simfphys/client/tab.lua")
@@ -35,3 +33,55 @@ include("simfphys/anim.lua")
 include("simfphys/base_lights.lua")
 include("simfphys/base_vehicles.lua")
 include("simfphys/view.lua")
+
+
+local lvsCars = {}
+
+hook.Add( "PreRegisterSENT", "!!!!!!lvs_register_simfphys_vehicles", function( ent, class )
+	if class ~= "gmod_sent_vehicle_fphysics_base" then return end
+
+	for name, data in pairs( list.Get( "simfphys_vehicles" ) ) do
+		if not istable( data.Members ) then continue end
+
+		local ENT = {}
+
+		ENT.Base = "gmod_sent_vehicle_fphysics_base"
+
+		ENT.MDL = data.Model
+
+		ENT.PrintName = data.Name
+		ENT.Author = data.Author
+		ENT.Category = "[LVS] - Cars"
+
+		ENT.Spawnable		= data.Spawnable ~= false
+		ENT.AdminSpawnable = data.AdminSpawnable == true
+
+		lvsCars[ name ] = true
+
+		scripted_ents.Register( ENT, name )
+	end
+end )
+
+hook.Add( "OnEntityCreated", "!!!!lvs_just_in_time_table_merge", function( ent )
+	if not IsValid( ent ) then return end
+
+	local class = ent:GetClass()
+
+	if not lvsCars[ class ] then return end
+
+	local vlist = list.Get( "simfphys_vehicles" )[ class ]
+
+	local data = vlist.Members
+
+	ent.MaxHealth = data.MaxHealth or 500
+
+	if SERVER then
+		local Z = ( vlist.SpawnOffset or Vector(0,0,0) ).z
+
+		ent:SetPos( ent:GetPos() + Vector(0,0,50 + Z) )
+
+		ent:SetAngles( ent:GetAngles() + Angle(0,(vlist.SpawnAngleOffset or 0),0) )
+
+		ent:InitFromList( data )
+	end
+end )
